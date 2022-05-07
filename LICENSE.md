0ce68db179e2f7fdbe44298375dc8823e78dcd9a
Damien Clauzel <Damien@Clauzel.eu>
2022-05-08 00:15:41 +0200

    commit initial

---
+++ LICENSE.md
@@ -0,0 +1,1 @@
+En ce glorieux jour du 2022-05-07, moi Damien Clauzel place naturellement ce dépôt sous la licence « [Fais pas chier](https://clauzel.eu/FPC/) ».
---
+++ Zookeeper/task/zookeeper/tmp.py
@@ -0,0 +1,23 @@
+x = 25
+while x <= 30:
+    print(x)
+    x += 3
+print(x)
+
+x = 25
+while x * 2 % 100 >= 50:
+    print(x)
+    x += 3
+print(x)
+
+x = 25
+while x % 2 != 0:
+    print(x)
+    x += 3
+print(x)
+
+x = 25
+while x > 0:
+    print(x)
+    x += 3
+print(x)
